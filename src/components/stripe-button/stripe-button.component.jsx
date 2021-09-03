c539cf9c57e7ac4adefdbb13df4aa166298f272b
August Felso <augustfelso@gmail.com>
2021-09-03 11:21:05 -0400

    Added stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+
+  const publishableKey = "pk_test_CiNPBIECrv6TZqX4Q2VE0jQN00g26eaYyO";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="React Demo Shop"
+      billingAddress
+      shippingAddress
+      img="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
