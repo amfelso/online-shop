df275b862a2d37e111450add2a24697d156cb92f
August Felso <augustfelso@gmail.com>
2021-11-16 15:47:29 -0500

    Change styles for homepage for mobile responsiveness

--- client/src/components/menu-item/menu-item.styles.js
+++ client/src/components/menu-item/menu-item.styles.js
@@ -51,6 +51,10 @@ export const ContentContainer = styled.div`
   background-color: white;
   opacity: 0.7;
   position: absolute;
+
+  @media screen and (max-width: 800px) {
+    width: 140px;
+  }
 `;
 
 export const ContentTitle = styled.span`
